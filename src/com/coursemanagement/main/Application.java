d8c1972bca7f9556f7267ba5526d1f248c6fbaaf
Mohamed Adel <mohamed.adel.mohamed.dev@gmail.com>
2026-07-25 13:42:34 +0300

    Initialize native Java project

---
+++ src/com/coursemanagement/main/Application.java
@@ -0,0 +1,9 @@
+package com.coursemanagement.main;
+
+public class Application {
+
+    public static void main (String[]args){
+        System.out.println("Course Enrollment Management System");
+        System.out.println("Application started successfully");
+    }
+}
